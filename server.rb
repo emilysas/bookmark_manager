901236f011f118d55555f7ce19b7e44ea7ac7bc9
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-13 17:05:49 +0000

    passes test - ability to sign up a new user with password

--- server.rb
+++ server.rb
@@ -18,6 +18,17 @@ DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
+
+  end
+  
   get '/' do
     @links = Link.all
     erb :index
@@ -42,9 +53,12 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    User.create(:email => params[:email],
+    user = User.create(:email => params[:email],
                 :password => params[:password])
+    session[:user_id] = user.id
     redirect to('/')
   end
+
+  
 
 end
