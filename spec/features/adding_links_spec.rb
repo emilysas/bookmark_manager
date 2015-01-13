6c4b02e776044b0eac53fef58d0b674723a9629b
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-13 16:28:48 +0000

    fourth rspec test passed

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -18,15 +18,15 @@ feature "User adds a new link" do
             "Makers Academy",
             ['education', 'ruby'])
     link = Link.first
-    expect(link.tags).to include("education")
-    expect(link.tags).to include("ruby")
+    expect(link.tags.map(&:text)).to include("education")
+    expect(link.tags.map(&:text)).to include("ruby")
   end
 
   def add_link(url, title, tags = [])
     within('#new-link') do
       fill_in 'url', :with => url
       fill_in 'title', :with => title
-      fill_in 'tags', :with => tags.join('')
+      fill_in 'tags', :with => tags.join(' ')
       click_button 'Add link'
     end
   end
