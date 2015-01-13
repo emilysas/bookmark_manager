d9f5be1ad8aebb385bc898f057d5c6da186aba8d
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-13 14:13:08 +0000

    creating necessary files and directories for sinatra bookmarkmanager  application

---
+++ server.rb
@@ -0,0 +1,15 @@
+require 'data_mapper'
+
+env = ENV['RACK_ENV'] || 'development'
+
+# we're telling datamapper to use a postgres database on localhost. bookmark_manager_test/development
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link' # this needs to be done after datamapper is initialised
+
+# After declaring your modesl, you should finalise them
+DataMapper.finalize
+
+# However, the database tables don't exist yet. Let's tell datamapper to create them
+DataMapper.auto_upgrade!
+
