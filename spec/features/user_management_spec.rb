65d79e34fbc3b13a48a84e3624b777eb4a37b7fd
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-14 10:07:57 +0000

    tests pass - will not sign up new user if password not confirmed

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -21,6 +21,7 @@ feature "User signs up" do
               password = "oranges!",
               password_confirmation = "oranges!")
     visit '/users/new'
+    expect(page.status_code).to eq(200)
     fill_in :email, :with => email
     fill_in :password, :with => password
     fill_in :password_confirmation, :with => password_confirmation
